f559b5b1c7e7c58431550709da6543684b60917c
JershDervis <joshdavis16@outlook.com>
2022-09-05 18:19:35 +1000

    Fix animation scroll appearing on load

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import { useEffect, useState } from 'react';
 
 //TODO: Find better way to set isPageLoaded state
+// preferably not using 2x useEffect blocks
 
 const Home: NextPage = () => {
 	const [isLoaded, setIsLoaded] = useState(false);
@@ -29,16 +30,20 @@ const Home: NextPage = () => {
 			<div className="w-full h-screen static bg-cover bg-[url('/bg.jpg')]">
 				<div className="w-full h-screen static bg-slate-900 bg-opacity-80">
 					<main
-						className={`container mx-auto flex flex-col items-center justify-center min-h-screen p-4 duration-700 transform ease-in transition-all ${
-							isPageLoaded ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'
-						}`}
+						className={`container mx-auto flex flex-col items-center justify-center min-h-screen p-4`}
 					>
-						<h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-200 text-center">
-							Josh <span className="text-blue-400">Davis</span> Codes
-						</h1>
-						<p className="text-2xl text-gray-300 text-center">
-							A portfolio of my developer experience
-						</p>
+						<div
+							className={`absolute duration-700 transform ease-in transition-all ${
+								isPageLoaded ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'
+							}`}
+						>
+							<h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-200 text-center">
+								Josh <span className="text-blue-400">Davis</span> Codes
+							</h1>
+							<p className="text-2xl text-gray-300 text-center">
+								A portfolio of my developer experience
+							</p>
+						</div>
 					</main>
 				</div>
 			</div>
